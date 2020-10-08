15ce6b0a81f4365f65497d6d386eceaa2e84daa1
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-10-08 11:05:35 +0530

    Final Commit

--- src/app/services/users-list-resolver.service.ts
+++ src/app/services/users-list-resolver.service.ts
@@ -10,7 +10,6 @@ export class UsersListResolverService implements Resolve<any>{
 
   constructor(private userService: UserService) { }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any | Observable<any> | Promise<any> {
-    console.log('In Resovler');
     return this.userService.getAllUsers();
   }
 }
